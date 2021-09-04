2d67f48d3147c26ba9c8e22c13d5fd1737f09c59
David Dikman <ddikman@gmail.com>
2021-09-04 16:30:32 +0900

    Fixed signup

--- functions/register/main.py
+++ functions/register/main.py
@@ -1,23 +1,38 @@
-from flask import abort
+from flask import abort, Response
 import validators
 from storage import register_new_user, UserAlreadyAdded
+from werkzeug.exceptions import BadRequest
+
+def bad_request(message, headers):
+  raise BadRequest(message, Response(message, 400, headers=headers))
 
 def register(request):
+    if request.method == 'OPTIONS':
+      headers = {
+              'Access-Control-Allow-Origin': '*',
+              'Access-Control-Allow-Methods': 'POST, OPTION',
+              'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+              'Access-Control-Max-Age': '3600'
+          }
+      return ('', 204, headers)
+
+    headers = { 'Access-Control-Allow-Origin': '*' }
+
     query = request.args.get('query')
     rank_site = request.args.get('rank_site')
     user = request.args.get('user')
 
     if not user or not query or not rank_site:
-      abort(400, 'query, rank_site and user must be specified')
+      bad_request('query, rank_site and user must be specified', headers)
 
     if not validators.domain(rank_site):
-      abort(400, f'rank_site is not a valid domain: {rank_site}')
+      bad_request(f'rank_site is not a valid domain: {rank_site}', headers)
 
     if not validators.email(user):
-      abort(400, f'user is not a valid email: {user}')
+      bad_request(f'user is not a valid email: {user}', headers)
 
     try:
       register_new_user(username=user, keyword=query, rank_site=rank_site)
-      return 'ok'
+      return ('ok', 200, headers)
     except UserAlreadyAdded:
-      return 'already added'
+      return ('already added', 200, headers)
