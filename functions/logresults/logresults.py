b976d00b56a00f3845d18e5b3501a91d0f01d89d
David Dikman <ddikman@gmail.com>
2021-09-04 18:13:15 +0900

    Added missing files

---
+++ functions/logresults/logresults.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from logresults.storage import bill_user, log_latest_result
+from firebase_admin import firestore
+from common.firebase import init_firebase
+
+init_firebase()
+
+def log_ranking_results(event, context):
+  if not 'data' in event:
+    print("No data in event, skipping")
+    return
+
+  import base64
+  import json
+
+  db = firestore.client()
+
+  message = json.loads(base64.b64decode(event['data']).decode('utf-8'))
+  user = message['user']
+  keyword = message['keyword']
+  rank_site = message['rank_site']
+  timestamp = datetime.fromisoformat(message['timestamp'])
+  rank = message['position']
+
+  log_latest_result(db, user, keyword, rank_site, rank, timestamp)
+  bill_user(db, user)
