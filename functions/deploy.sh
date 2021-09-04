359cedfee7229fb8b59d676e26bdf83b49b9b462
David Dikman <ddikman@gmail.com>
2021-09-04 18:52:28 +0900

    Removed superfluous project switch

--- functions/deploy.sh
+++ functions/deploy.sh
@@ -6,8 +6,6 @@ cd $SCRIPT_DIR
 echo "Running deploy from $SCRIPT_DIR"
 
 PROJECT=seoranking-324303
-gcloud config set project $PROJECT
-echo "Switched to $PROJECT"
 
 echo "Deploying.."
 gcloud functions deploy get_stats --entry-point get_stats_http --project $PROJECT --runtime python37 --trigger-http
