32b72800ef3d8ba3797deaf0e5d0aa8170afbefe
David Dikman <ddikman@gmail.com>
2021-09-23 19:26:25 +0900

    Updated ranking email

--- functions/notifyranking/notifyranking.py
+++ functions/notifyranking/notifyranking.py
@@ -9,7 +9,8 @@ def notify_ranking(event, context):
   keyword = data['keyword']
   user = data['user']
 
-  if not rank_changed(user, site, keyword, position):
+  last_ranking = get_last_ranking(user, keyword, site)
+  if last_ranking == position:
     print(f"Rank for site '{site}' on '{keyword}' is still ranking {position}, skipping email notification")
     return
 
@@ -17,19 +18,23 @@ def notify_ranking(event, context):
     'from': 'david@greycastle.se',
     'to': user,
     'subject': f'New ranking update for {site}',
-    'html_content': build_email(position, site, keyword)
+    'html_content': build_email(last_ranking, position, site, keyword)
   })
 
-def rank_changed(user, site, keyword, position):
-  last_ranking = get_last_ranking(user, keyword, site)
-  return last_ranking != position
+def build_rank_change(prev_rank, new_rank):
+  if prev_rank < 0:
+    return f"Your site ranking has been confirmed, you are ranking at position {prev_rank}"
 
-def build_email(ranking, site, keyword):
+  return f"Your rank has changed from {prev_rank} to {new_rank}."
+
+def build_email(last_ranking, ranking, site, keyword):
+
+  rank_change_message = build_rank_change(last_ranking, ranking)
   return f"""
   Hi!
   <p>Your rank for {site} and keywords '{keyword}' was just updated.</p>
 
-  <p>You are now ranking on {ranking} place.</p>
+  <p>{rank_change_message}</p>
 
   <p>Check out your current and new rankings on the <a href="https://rank.greycastle.se/history.html">History page</a>.</p>
 
