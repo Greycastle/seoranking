a8423fe62490a5618b2305d1d478a52bafc7c853
David Dikman <ddikman@gmail.com>
2021-09-23 16:38:40 +0900

    Added missing changes to stats

--- functions/stats/stats.py
+++ functions/stats/stats.py
@@ -1,6 +1,7 @@
-from common.http import not_found, enable_cors, require_auth
+from common.http import not_found, bad_request, enable_cors, require_auth
 from stats.data_source import read_stats, NoSuchUser, read_detailed_stats, NoSuchEntry
 from common.firebase import init_firebase
+import base64
 import logging
 
 logger = logging.getLogger(__name__)
@@ -18,7 +19,13 @@ def get_stats(request):
 @enable_cors
 def get_public_stats(request):
   try:
-    return read_stats(request.args.get['email'])
+    id = request.args.get('id', None)
+    if id is None:
+      bad_request('Missing id attribute')
+    email = base64.b64decode(id.encode()).decode()
+    stats = read_stats(email)
+    stats.pop('stats')
+    return stats
   except(NoSuchUser):
     not_found('No such user found')
 
